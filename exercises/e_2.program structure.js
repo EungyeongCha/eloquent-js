e079f37af015de5f6a081c26be5e2000d3407c44
eungyungcha <eungyungcha@gmail.com>
2020-07-24 10:22:45 +0900

    complete chp2

--- exercises/e_2.program structure.js
+++ exercises/e_2.program structure.js
@@ -13,11 +13,11 @@ Write a loop that makes seven calls to console.log to output the following trian
 */
 
 
-let container = ""
+/*let container = ""
 for (let number = 1; number <= 7; number++) {
     container += "#";
     console.log(container);    
-}
+}*/
 
 /*
 FizzBuzz
@@ -27,6 +27,7 @@ Write a program that uses console.log to print all the numbers from 1 to 100, wi
 When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 */
 
+/*
 for (let num = 1; num <= 100; num++) {
     if (num % 3 == 0 && num % 5 == 0) {
         console.log("FizzBuzz");
@@ -38,6 +39,7 @@ for (let num = 1; num <= 100; num++) {
         console.log(num);
     }
 }
+*/
 
 /*
 Chessboard
@@ -57,16 +59,30 @@ Passing this string to console.log should show something like this:
 When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
 */
 
-let eptStr = " ";
-for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
+let eptStr = "";
+function dash(n) {
+    for (let i = 0; i < n; i++) {
+    if (i % 2 == 0) {
+        for (let j = 0; j < n; j++) {
+        if (j % 2 == 0) {
+            eptStr += " ";
+        } else {
+            eptStr += "#";
+        }
+    }
+    }
+    else {
+        for (let j = 0; j < n; j++) {
         if (j % 2 == 0) {
             eptStr += "#";
         } else {
             eptStr += " ";
-        }
-        eptStr += "\n";
+        }       
     }
-    console.log(eptStr);
+   }
+    eptStr += "\n";
+}
+console.log(eptStr);
 }
 
+dash(10);
