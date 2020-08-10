2c3763750b1cb9d60e96d24c80abb71e431feac8
eungyungcha <eungyungcha@gmail.com>
2020-08-10 17:33:59 +0900

    work on n_7

--- notetaking/n_7.project_a robot.js
+++ notetaking/n_7.project_a robot.js
@@ -35,4 +35,87 @@ const roads = [
 
 
   // 2. The task
-  
+  // the viallage's state to the minimal set of values that define it
+  // (robot's current situation, the collection of undelivered parcel, each of which has a current location and a destination address)
+
+  // where the robot is and where the parcels are
+  class VillageState {
+      constructor(place, parcels) {
+          this.place = place;
+          this.parcels = parcels;
+      }
+      // check if there is a road it can go and return old state if not
+      // creates new state with the destination as the robot's new palce
+      // map: move, filter: deliver
+      move(destination) {
+        if(!roadGraph[this.place].includes(destination)) {
+            return this;
+        } else {
+            let parcels = this.parcels.map(p => {
+                if (p.place != this.place) return p;
+                return {place: destination, address: p.place};
+            }).filter(p => p.place != p.address);
+            return new VillageState(destination, parcels);
+        }
+      }
+  }
+
+  let first = new VillageState(
+      "Post Office",
+      [{place: "Post Office", address: "Alice's House"}]
+  );
+  let next = first.move("Alice's House");
+
+  console.log(next.place);
+  // -> Alice's House
+  console.log(next.parcels);
+  // -> []
+  console.log(first.place);
+  // -> Post Office
+
+
+
+  // 3. Persistanct data
+  let object = Object.freeze({value: 5});
+  object.value = 10;
+  console.log(object.value);
+  // ->5
+
+
+
+  // 4. Simulation
+  // a robot is a function that takes a VillageState object and returns the name of a nearby place
+  function runRobot(state, robot, memory) {
+      for (let turn = 0;; turn++) {
+          if (state.parcels.length == 0) {
+              console.log( `Done in ${turn} turns`);
+              break;
+          }
+          let action = robot(state, memory);
+          state = state.move(action.direction);
+          memory = action.memory;
+          console.log(`Moved to ${action.direction}`);
+      }
+  } 
+
+  function randomPick(array) {
+      let choice = Math.floor(Math.ramdom() * array.length);
+      return array[choice];
+  }
+
+  function randomRobot(state) {
+      return {direction: randomPick(roadGraph[state.place])};
+  }
+
+  VillageState.random = function(parcelCount = 5) {
+      let parcels = [];
+      for (let i = o; i < parcelCount; i++) {
+          let address = randomPick(Object.keys(roadGraph));
+          let place;
+          do {
+              place = randomPick(Object.keys(roadGraph));
+          } while (place == address);
+          parcels.push({place, address});
+      }
+      return new VillageState("Post Office", parcels)
+  }
