2f0e1214d9f749627111a90511139204129f851b
eungyungcha <eungyungcha@gmail.com>
2020-08-12 23:26:41 +0900

    work on n_8

---
+++ notetaking/n_8.bugs and errors.js
@@ -0,0 +1,27 @@
+// 1. Strict mode
+function canYouSpotTheProblem() {
+    "use strict";
+    for (counter = 0; counter < 10; counter++) {
+        console.log("Happy happy");
+    }
+}
+canYouSpotTheProblem();
+//-> ReferenceError: counter is not defined
+
+
+
+function Person(name) {this.name = name;}
+let ferdinand = Person("Ferdinand"); // forgot new
+console.log(name);
+// -> Ferdinand
+
+
+"use strict";
+function Person(name) {this.name = name;}
+ler ferdinand = Person("Ferdinand"); // forgot new
+// -> TypeError: Cannot set property 'name' of undefined
+
+// class notation returns error when called without new
+
+
+
